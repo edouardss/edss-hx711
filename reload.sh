e7dafc22326bd0fdc4eb0ae9eb186a49388ed22d
edouardss <edouardss@gmail.com>
2025-09-10 13:10:54 +0100

    disabling global logging

--- reload.sh
+++ reload.sh
@@ -3,6 +3,12 @@
  # bash safe mode. look at `set --help` to see what these are doing
  set -euxo pipefail
 
+# Set log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+export VIAM_LOG_LEVEL=INFO
+
+# Disable the logging configuration entirely
+export VIAM_DISABLE_LOGGING_CONFIG=true
+
  cd $(dirname $0)
  MODULE_DIR=$(dirname $0)
  VIRTUAL_ENV=$MODULE_DIR/venv
