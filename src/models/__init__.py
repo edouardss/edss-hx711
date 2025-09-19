ba9386be8fef19297e6f6dfd5e09652cd277a855
edouardss <edouardss@gmail.com>
2025-09-19 23:34:16 +0100

    fixing versions required

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,5 +2,6 @@
 
 from .loadcell import Loadcell
 from .bmp_sensor import BmpSensor
+from .imu_sensor import ImuSensor
 
-__all__ = ["Loadcell", "BmpSensor"]
+__all__ = ["Loadcell", "BmpSensor", "ImuSensor"]
